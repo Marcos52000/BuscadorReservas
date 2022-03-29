1348940e9f13992803c096cda70187e9902cc2e4
TheBerayita <97508463+TheBerayita@users.noreply.github.com>
2022-03-29 22:40:25 +0200

    Update SimpleCORSFilter.java

--- src/main/java/com/security/SimpleCORSFilter.java
+++ src/main/java/com/security/SimpleCORSFilter.java
@@ -27,9 +27,9 @@ public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
     HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) res;
 
-    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+    response.setHeader("Access-Control-Allow-Origin", request.getHeader("/**"));
     response.setHeader("Access-Control-Allow-Credentials", "true");
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT,HEAD");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, HEAD");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Host, Content-Lenght");
 
